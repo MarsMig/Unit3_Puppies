cba7677c717ff1e72fedd0eaed7e70c180acfedd
MarsMig <mars.n.m@outlook.com>
2024-10-22 11:51:20 -0400

    puppies are clicking to view by Id

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,36 @@ import "./App.css";
 import { puppyList } from "./data.js";
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  function handleClick(id) {
+    // some logic here
+    const featuredPup = puppies.find((pup) => pup.id === featPupId);
+
+    console.log(featuredPup);
+
+    console.log("puppy id: ", id);
+  }
+
   console.log(puppies);
   return (
     <>
       <div className="App">
         <div>
           {puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>;
+            return (
+              <p
+                onClick={() => {
+                  setFeatPupId(puppy.id);
+                }}
+                key={puppy.id}
+              >
+                {puppy.name}
+              </p>
+            );
           })}
         </div>
+        {featPupId && <p>{featPupId}</p>}
       </div>
     </>
   );
