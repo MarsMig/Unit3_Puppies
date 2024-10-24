b37feafcb355444f7cbd81e7e1aeccf597ea648f
MarsMig <mars.n.m@outlook.com>
2024-10-24 09:06:21 -0400

    all jsx base code implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import "./App.css";
 import { puppyList } from "./data.js";
+import "./path-to-css.css";
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
-
   function handleClick(id) {
     // some logic here
     const featuredPup = puppies.find((pup) => pup.id === featPupId);
@@ -13,7 +13,8 @@ function App() {
 
     console.log("puppy id: ", id);
   }
-
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
   console.log(puppies);
   return (
     <>
@@ -32,7 +33,15 @@ function App() {
             );
           })}
         </div>
-        {featPupId && <p>{featPupId}</p>}
+        {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
       </div>
     </>
   );
